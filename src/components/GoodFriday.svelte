08e026f6abc30656596da673749e6a8c11d5eefc
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2022-04-01 07:54:42 -0600

    fix good friday issues

--- src/components/GoodFriday.svelte
+++ src/components/GoodFriday.svelte
@@ -1,10 +1,34 @@
 <script>
-	import Block from '../components/block.svelte';
+	import Block from '$components/block.svelte';
 	let blockClass = 'GoodFriday';
 	let id = 'GoodFriday';
-	import Img from '../components/image.svelte';
+	import Img from '$components/image.svelte';
 </script>
 
 <Block {blockClass} {id}>
-	<h1>Good Friday</h1>
+	<div class="GFcontainer">
+		<h1>Good Friday</h1>
+		<div class="stationsContainer">
+			<div class="imgContainer"><Img source="station2" altTag="Good Friday" /></div>
+			<div class="imgContainer"><Img source="station10" altTag="Good Friday" /></div>
+			<div class="imgContainer"><Img source="station11" altTag="Good Friday" /></div>
+		</div>
+	</div>
 </Block>
+
+<style lang="scss">
+	.stationsContainer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 90%;
+		margin: 0 auto;
+		padding: 0;
+		gap: 5vw;
+
+		@media (max-width: 620px) {
+			flex-direction: column;
+			gap: 30px;
+		}
+	}
+</style>
